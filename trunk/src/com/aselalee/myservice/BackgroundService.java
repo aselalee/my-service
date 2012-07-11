817972bff3b1ce999fb4a739f9672e49c9dddcd7
aselalee@gmail.com <aselalee@gmail.com@0d1fcb35-7899-d8b1-b092-173254ef1ea7>
2012-07-11 04:56:03 +0000

    Initial source commit

---
+++ trunk/src/com/aselalee/myservice/BackgroundService.java
@@ -0,0 +1,98 @@
+package com.aselalee.myservice;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class BackgroundService extends Service implements MyListener{
+	private MyThread myServiceThread = null;
+	private Notification mNt = null;
+	private static boolean isMyServiceStarted;
+	private final int ST_L1 = 0;
+	private final int ST_L3 = 2;
+	private static MyListener myListener;
+	private NotificationManager mNM = null;
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		isMyServiceStarted = false;
+		return START_STICKY;
+	}
+
+	private void setNotification() {
+		mNt = new Notification(R.drawable.status_level,
+				"MyService Ticker Text", System.currentTimeMillis());
+		mNt.iconLevel = ST_L1;
+		Intent mIn = new Intent(this, MainActivity.class);
+		PendingIntent mPIn = PendingIntent.getActivity(this, 0, mIn, 0);
+		mNt.setLatestEventInfo(this, "MyService Content Title", "MyService Content Text", mPIn);
+		startForeground(1, mNt);
+	}
+
+	private void toggleNotificationIcon() {
+		mNt.iconLevel++;
+		if(mNt.iconLevel > ST_L3) {
+			mNt.iconLevel = ST_L1;
+		}
+		mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+		mNM.notify(1, mNt);
+	}
+
+	@Override
+	public void onDestroy() {
+		Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
+		if(myServiceThread != null) {
+			myServiceThread.interrupt();
+		}
+		isMyServiceStarted = false;
+		stopForeground(true);
+		myListener = null;
+	}
+
+	@Override
+	public void onCreate() {
+		if(isMyServiceStarted == true){
+			return;
+		}
+		setNotification();
+		Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+		myServiceThread = new MyThread();
+		myServiceThread.start();
+		isMyServiceStarted = true;
+		myListener = this;
+	}
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return null;
+	}
+
+	private class MyThread extends Thread {
+		@Override
+		public void run() {
+			super.run();
+			while(true) {
+				try {
+					Thread.sleep(1000);
+					Log.i("MyService", "Running...\n");
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					break;
+				}
+			}
+		}
+	}
+
+	@Override
+	public void onStatusChange(int statusID, String statusMsg) {
+		toggleNotificationIcon();
+	}
+
+	public static MyListener GetMyListener() {
+		return myListener;
+	}
+}
---
+++ trunk/src/com/aselalee/myservice/MainActivity.java
@@ -0,0 +1,39 @@
+package com.aselalee.myservice;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+	private MyListener myListener = null;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void startMyService(View v) {
+		Intent intent = new Intent(this, BackgroundService.class);
+		startService(intent);
+	}
+
+	public void stopMyService(View v) {
+		stopService(new Intent(Intent.ACTION_MAIN).setClass(this, BackgroundService.class));
+	}
+
+	public void toggleNotificaton(View V) {
+		myListener = BackgroundService.GetMyListener();
+		if(myListener != null)
+			myListener.onStatusChange(0, "My Message");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
